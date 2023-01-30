23801a94fa2a625d8f3b60f362336c1309ce7a2e
Pramodh <Pramodh.p@datasturdy.com>
2023-01-30 11:58:43 +0000

    First comment

---
+++ Version/Version/dbo/Stored Procedures/demo.sql
@@ -0,0 +1,9 @@
+﻿CREATE proc [dbo].[demo]
+as
+select * from sam1 where id between 20 and 30 ;
+with samcte
+as
+(
+select * from sam1 where id between 5 and 19
+)
+select * from samcte
---
+++ Version/Version/dbo/Stored Procedures/sam.sql
@@ -0,0 +1,3 @@
+﻿CREATE proc [dbo].[sam]
+as
+select * from sam1 where id between 15 and 20
